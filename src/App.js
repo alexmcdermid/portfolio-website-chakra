1c41ba123f93d526e62a3233e1c853e55ea7211d
alexmcdermid <alexander.mcdermid@gmail.com>
2021-11-24 18:26:31 -0800

    make the top bar icons and text change based on mobile

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import { VStack, Flex, Heading, IconButton, useColorMode, Spacer } from "@chakra-ui/react";
 import {FaSun, FaMoon, FaGithub, FaLinkedin, FaEnvelope} from 'react-icons/fa'
+import { useMediaQuery } from '@chakra-ui/media-query';
 import Content from "./components/Content";
 
 function App() {
 
   const {colorMode,toggleColorMode} = useColorMode();
   const isDark = colorMode === 'dark'
+  const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)");
 
   return (
     <VStack padding='4'>
       <Flex w='100%'>
-        <Heading m1='8' size='md' fontWeight='semibold' color='orange.400'>
+        <Heading m1='8' size={isNotSmallerScreen ? 'lg' : 'md'} fontWeight='semibold' color='orange.400'>
           alex.dev 
         </Heading>
         <Spacer/>
-        <IconButton icon={<FaLinkedin/>} isRound='true' onClick={() => window.open("https://www.linkedin.com/in/alexmcdermid")}></IconButton>
-        <IconButton icon={<FaEnvelope/>} isRound='true' onClick={() => window.open("mailto:alexander.mcdermid@gmail.com")}></IconButton>
-        <IconButton icon={<FaGithub/>} isRound='true' onClick={() => window.open("https://www.github.com/alexmcdermid")}></IconButton>
-        <IconButton icon={isDark ? <FaSun/> : <FaMoon/>} isRound='true' onClick={toggleColorMode} />
+        <IconButton icon={<FaLinkedin />} size={isNotSmallerScreen ? 'lg' : 'md'} isRound='true' onClick={() => window.open("https://www.linkedin.com/in/alexmcdermid")}></IconButton>
+        <IconButton icon={<FaEnvelope/>} size={isNotSmallerScreen ? 'lg' : 'md'} isRound='true' onClick={() => window.open("mailto:alexander.mcdermid@gmail.com")}></IconButton>
+        <IconButton icon={<FaGithub/>} size={isNotSmallerScreen ? 'lg' : 'md'} isRound='true' onClick={() => window.open("https://www.github.com/alexmcdermid")}></IconButton>
+        <IconButton icon={isDark ? <FaSun/> : <FaMoon/>} size={isNotSmallerScreen ? 'lg' : 'md'} isRound='true' onClick={toggleColorMode} />
       </Flex>
       <Content></Content>
     </VStack>
